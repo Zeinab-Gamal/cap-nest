3b3225df6a9c9b50da51a1e713ed824b2ddd130b
nahla-ibrahim <nahlaibrahim2024@gmail.com>
2026-01-18 15:34:24 +0200

    configure assets and install Font Awesome

--- src/app/components/hero-section/hero-section.ts
+++ src/app/components/hero-section/hero-section.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlay, faArrowRight } from '@fortawesome/free-solid-svg-icons'; // Import specific icons
 
 @Component({
   selector: 'app-hero-section',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './hero-section.html',
   styleUrl: './hero-section.css',
 })
 export class HeroSection {
-
+  arrowRightIcon = faArrowRight;
+  playIcon = faPlay;
 }

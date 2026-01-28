4fcadb860c90c7af43ca47bb505928a2e12c3cbe
nahla-ibrahim <nahlaibrahim2024@gmail.com>
2026-01-28 15:26:20 +0200

    add smooth scroll for navbar links

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, signal } from '@angular/core';
 import { NgClass } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faGlobe, faBars } from '@fortawesome/free-solid-svg-icons';
+import { log } from 'console';
 @Component({
   selector: 'app-navbar',
   imports: [NgClass, FontAwesomeModule],
@@ -11,10 +12,10 @@ import { faGlobe, faBars } from '@fortawesome/free-solid-svg-icons';
 export class Navbar {
   navLinks = [
     { name: 'How It Works', href: '#how-it-works' },
-    { name: 'Why CapNest', href: '#' },
+    { name: 'Why CapNest', href: '#benefits' },
     { name: 'Properties', href: '#properties' },
-    { name: 'Join Us', href: '#about' },
-    { name: 'Testimonials', href: '#benefits' },
+    { name: 'Join Us', href: '#joinus' },
+    { name: 'Testimonials', href: '#testimonials' },
   ];
 
   isScrolled = signal<boolean>(false);
@@ -28,5 +29,8 @@ export class Navbar {
   }
   toggleMobileMenu() {
     this.isMobileMenuOpen.set(!this.isMobileMenuOpen());
+  }
+  goToSection(id: string) {
+    document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
   }
 }

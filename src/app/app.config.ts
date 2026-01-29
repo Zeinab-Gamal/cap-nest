0a3fef79c18ea696b8e743c0fb6d42acb4219716
Samar Ibrahim <samaribrahim@Samars-MacBook-Air.local>
2026-01-29 22:26:24 +0300

    fix translate config for deploy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, PLATFORM_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -7,19 +7,31 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import {provideTranslateService} from "@ngx-translate/core";
 import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
 import { provideHttpClient } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideHttpClient(),
-    provideTranslateService({
-      loader: provideTranslateHttpLoader({
-        prefix: '/assets/i18n/',
-        suffix: '.json'
-      }),
-      fallbackLang: 'en',
-      lang: 'en'
-    })
+    {
+      provide: 'TRANSLATE_BROWSER_ONLY',
+      useFactory: () => {
+        const platformId = inject(PLATFORM_ID);
+
+        if(isPlatformBrowser(platformId)) {
+          return provideTranslateService({
+            loader: provideTranslateHttpLoader({
+              prefix: '/assets/i18n/',
+              suffix: '.json'
+            }),
+            fallbackLang: 'en',
+            lang: 'en'
+          });
+        }
+
+        return [];
+      }
+    }
   ]
 };
